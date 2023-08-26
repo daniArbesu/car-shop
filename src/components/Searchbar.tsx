1dd39ecfb92142d0ee797acc3e76cfd86a5108b4
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-26 11:06:23 +0200

    feat: add basic search structure

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,16 +1,49 @@
 'use client';
 import { useState } from 'react';
 import SearchManufacturer from './SearchManufacturer';
+import { SearchIcon } from './ui/Icons';
+import Image from 'next/image';
+
+const SearchButton = ({ className }: { className: string }) => (
+  <button type="submit" className={`z-10 -ml-3 ${className}`}>
+    <SearchIcon />
+  </button>
+);
 
 const Searchbar = () => {
   const [manufacturer, setManufacturer] = useState('');
-  const handleSearch = () => {};
+  const [model, setModel] = useState('');
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === '' || model === '') return;
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
+        <SearchButton className="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="Car Model icon"
+          width={25}
+          height={25}
+          className="absolute ml-4 h-[20px] w-[20px]"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton className="sm:hidden" />
+      </div>
+      <SearchButton className="max-sm:hidden" />
     </form>
   );
 };
--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -116,3 +116,31 @@ export const CloseIcon: React.FC<Props> = ({ className }) => {
     </svg>
   );
 };
+
+export const SearchIcon: React.FC<Props> = ({ className }) => {
+  return (
+    <svg className="h-10 w-10" viewBox="0 0 40 40" fill="none">
+      <rect className="h-10 w-10" rx="20" fill="white" />
+      <path
+        d="M28 28L24.1333 24.1333M26.2222 19.1111C26.2222 23.0385 23.0385 26.2222 19.1111 26.2222C15.1838 26.2222 12 23.0385 12 19.1111C12 15.1838 15.1838 12 19.1111 12C23.0385 12 26.2222 15.1838 26.2222 19.1111Z"
+        stroke="url(#paint0_linear_1_2)"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_2"
+          x1="4.76191"
+          y1="9.71428"
+          x2="48.9524"
+          y2="39.0476"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5D5FC0" />
+          <stop offset="1" stopColor="#FFD2CC" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
